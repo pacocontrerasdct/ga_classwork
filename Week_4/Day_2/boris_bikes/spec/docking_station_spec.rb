9bd9610038f2c47c35c11898b39c448b49bd1501
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-01 14:47:33 +0100

    refactoring docking_station_spec

--- Week_4/Day_2/boris_bikes/spec/docking_station_spec.rb
+++ Week_4/Day_2/boris_bikes/spec/docking_station_spec.rb
@@ -2,29 +2,22 @@ require_relative '../lib/docking_station.rb'
 require_relative '../lib/bike.rb'
 
 describe DockingStation do
-  
-  it 'should be empty after we build it' do
-    station = DockingStation.new
+  let(:station) { DockingStation.new }
+  let(:bike) { Bike.new }
 
+  it 'should be empty after we build it' do
     expect(station.bike_count).to eq 0
   end
 
   it 'should be able to dock a bike' do
   # For pending this test: -> xit 'should be able to dock a bike' do
-    station = DockingStation.new
-    bike = Bike.new
     station.dock(bike)
-
     expect(station.bike_count).to eq 1
   end
 
   it 'should be able to release a bike' do
-    station = DockingStation.new
-    bike = Bike.new
-
     station.dock(bike)
     station.release(bike)
-
     expect(station.bike_count).to eq 0
   end
 
