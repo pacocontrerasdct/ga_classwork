cf3750876711cd65228ba52178d42178e2aa66a2
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-01 14:24:49 +0100

    fixed bike and others things

--- Week_4/Day_2/boris_bikes/spec/bike_spec.rb
+++ Week_4/Day_2/boris_bikes/spec/bike_spec.rb
@@ -2,22 +2,22 @@ require_relative '../lib/bike.rb'
 
 #We're describing functionality of the specific class, Bike
 describe Bike do
+  
+  # Instantiating an instance of the Bike class and refactored to be 'bike'
+  let(:bike) { Bike.new }
 
   it 'should not be broken after with create it' do
-    bike = Bike.new # Instantiate an instance of the Bike class
     expect(bike.broken?).to be false
     # Same: expect(bike.broken?).not_to be broken
   end
 
   it 'should be able to break' do
-    bike = Bike.new
     bike.break
-    
+
     expect(bike.broken?).to be true
   end
 
   it 'should be able to be fixed' do
-    bike = Bike.new
     bike.break
     bike.fix
 
---
+++ Week_4/Day_2/boris_bikes/spec/docking_station_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../lib/docking_station.rb'
+require_relative '../lib/bike.rb'
+
+describe DockingStation do
+  
+  it 'should be empty after we build it' do
+    station = DockingStation.new
+
+    expect(station.bike_count).to eq 0
+  end
+
+  it 'should be able to dock a bike' do
+  # For pending this test: -> xit 'should be able to dock a bike' do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+
+    expect(station.bike_count).to eq 1
+  end
+
+
+end
